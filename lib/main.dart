2d0d2c371795215b7d286176c1c660f16aae0e77
Erfan Ghasemi <erfanghasemiioogg@gmail.com>
2025-01-05 11:22:03 +0330

    create theme for whole project

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const String fontFamilyDefualt = "Avenir";
+  static const Color primaryTextColor = Color(0xff0D253C);
+  static const Color secondryTextColor = Color(0xff2D4379);
+  static const Color primaryColor = Color(0xff376AED);
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Blog Club',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        textButtonTheme: const TextButtonThemeData(
+            style: ButtonStyle(
+                textStyle: WidgetStatePropertyAll(
+          TextStyle(
+              fontFamily: fontFamilyDefualt,
+              color: primaryColor,
+              fontWeight: FontWeight.w400,
+              fontSize: 14),
+        ))),
+        colorScheme: const ColorScheme.light(
+          primary: primaryColor,
+          onPrimary: Colors.white,
+          background: Color(0xffFbFCFF),
+          surface: Colors.white,
+          onSurface: primaryTextColor,
+          onBackground: primaryTextColor,
+        ),
+        appBarTheme: AppBarTheme(
+            color: Theme.of(context).colorScheme.surface,
+            titleSpacing: 32,
+            foregroundColor: Theme.of(context).colorScheme.onBackground),
+        snackBarTheme: const SnackBarThemeData(
+          backgroundColor: primaryColor,
+        ),
+        useMaterial3: false,
+        textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+                fontFamily: fontFamilyDefualt,
+                color: secondryTextColor,
+                fontWeight: FontWeight.w700,
+                fontSize: 18),
+            bodyMedium: TextStyle(
+                color: secondryTextColor,
+                fontFamily: fontFamilyDefualt,
+                fontSize: 14),
+            titleSmall: TextStyle(
+                fontFamily: fontFamilyDefualt,
+                color: secondryTextColor,
+                fontWeight: FontWeight.w200,
+                fontSize: 18),
+            headlineLarge: TextStyle(
+                color: primaryTextColor,
+                fontFamily: fontFamilyDefualt,
+                fontSize: 24,
+                fontWeight: FontWeight.bold),
+            bodySmall: TextStyle(
+                color: secondryTextColor,
+                fontFamily: fontFamilyDefualt,
+                fontSize: 12),
+            labelSmall: TextStyle(
+                fontFamily: fontFamilyDefualt,
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w700),
+            headlineMedium: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+                color: primaryTextColor,
+                fontFamily: fontFamilyDefualt),
+            titleMedium: TextStyle(
+                fontFamily: fontFamilyDefualt,
+                color: primaryTextColor,
+                fontWeight: FontWeight.w400,
+                fontSize: 14),
+            displaySmall: TextStyle(
+                fontFamily: fontFamilyDefualt,
+                fontWeight: FontWeight.w700,
+                fontSize: 10,
+                color: secondryTextColor)),
       ),
       home: const HomeScreen(),
     );
